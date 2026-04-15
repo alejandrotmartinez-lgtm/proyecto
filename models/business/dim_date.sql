ab6eb9a41e8ccbc3d06e45de2b18bbcff5f7d8d9
Alejandro Tapia Martinez <alejandro.tmartinez@sdggroup.com>
2026-04-15 11:16:48 +0000

    prueba github video

---
+++ models/business/dim_date.sql
@@ -0,0 +1,15 @@
+with date_spine as (
+    {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="cast('1990-01-01' as date)",
+        end_date="cast('2000-01-01' as date)"
+    ) }}
+)
+select
+    date_day as date_id,
+    extract(year from date_day) as year,
+    extract(month from date_day) as month,
+    extract(day from date_day) as day,
+    extract(dayofweek from date_day) as day_of_week,
+    case when extract(dayofweek from date_day) in (0, 6) then true else false end as is_weekend
+from date_spine
